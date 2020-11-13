3050a39efebcbe6cd25ee6832c62a92c64b12ec4
JoseAntonioMartinez2525 <63381930+JoseAntonioMartinez2525@users.noreply.github.com>
2020-11-12 22:05:15 -0700

    Add files via upload

---
+++ p.php
@@ -0,0 +1,23 @@
+<?php
+	$name= "Jose Antonio";
+	$lastname="Martínez del Toro";
+	$completo="$name $lastname";
+	//comentario de linea
+	/*comentario*/
+	#comentario
+	?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<h1>Hola mundo,mi nombre es: </h1>
+	<?php
+		echo "<h1>$completo</h1>";
+	?>
+	
+</body>
+</html>
